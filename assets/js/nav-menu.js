d92bdd6e7a8a0893a1a4f36bcbf5e82be3279727
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-08 16:18:26 +0200

    System campaign services done

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -13,6 +13,37 @@ function closeSideBar() {
 	$(".sidebar-overlay").attr("navIsOpen", "false");
 }
 
+
+let subMenuIsVisible = false;
+const navMenu = document.querySelector(".nav-menu");
+const navMenuItems = document.querySelectorAll(".nav-menu-item");
+const navSubMenu = document.querySelector(".nav-sub-menu");
+
+console.log('=>', typeof navMenuItems);
+
+navMenuItems.forEach(item => {
+	console.log('ITEM', item);
+	if (!subMenuIsVisible) {
+		item.addEventListener('mouseover', function() {
+			console.log('Hovered!');
+			navSubMenu.classList.remove('is-hidden');
+			navSubMenu.classList.add('is-active');
+			subMenuIsVisible = true;
+		});
+	} else if (subMenuIsVisible) {
+		item.addEventListener('mouseout', function() {
+			console.log('Unhovered!');
+			navSubMenu.classList.remove('is-active');
+			navSubMenu.classList.add('is-hidden');
+			subMenuIsVisible = false;
+		});
+	}
+});
+//
+// navMenuItem
+//
+// navMenuItem
+
 // EOL: Igor
 
 
