71ea01077cdf8274ad8963d820eae109c7cb0c6d
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-07 10:42:44 +0300

    working on support page...

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -14,29 +14,29 @@ function closeSideBar() {
 }
 
 // Nav menu header
-const navMenuHeader = document.querySelector('.wrapper-c .nav-menu');
-const navMenuItems = document.querySelectorAll(".wrapper-c .nav-menu .nav-menu-item");
-const navSubMenu = document.querySelector(".wrapper-c .nav-sub-menu");
-const mainRow = document.querySelector('#main-row');
-let subMenuIsVisible = false;
-console.log('=>', navMenuHeader);
+// const navMenuHeader = document.querySelector('.wrapper-c .nav-menu');
+// const navMenuItems = document.querySelectorAll(".wrapper-c .nav-menu .nav-menu-item");
+// const navSubMenu = document.querySelector(".wrapper-c .nav-sub-menu");
+// const mainRow = document.querySelector('#main-row');
+// let subMenuIsVisible = false;
+// console.log('=>', navMenuHeader);
 
-navMenuItems.forEach(item => {
-	item.addEventListener('mouseover', function() {
+// navMenuItems.forEach(item => {
+// 	item.addEventListener('mouseover', function() {
+		// // console.log('=> Worked');
+		// navSubMenu.classList.remove('is-hidden');
+		// navSubMenu.classList.add('is-active');
+		// mainRow.classList.add('nav-bar-is-opened');
+		// subMenuIsVisible = true;
+	// });
+	// item.addEventListener('mouseout', function() {
 		// console.log('=> Worked');
-		navSubMenu.classList.remove('is-hidden');
-		navSubMenu.classList.add('is-active');
-		mainRow.classList.add('nav-bar-is-opened');
-		subMenuIsVisible = true;
-	});
-	item.addEventListener('mouseout', function() {
-		// console.log('=> Worked');
-		navSubMenu.classList.add('is-hidden');
-		navSubMenu.classList.remove('is-active');
-		mainRow.classList.remove('nav-bar-is-opened');
-		subMenuIsVisible = false;
-	});
-});
+		// navSubMenu.classList.add('is-hidden');
+		// navSubMenu.classList.remove('is-active');
+		// mainRow.classList.remove('nav-bar-is-opened');
+		// subMenuIsVisible = false;
+// 	});
+// });
 
 
 
