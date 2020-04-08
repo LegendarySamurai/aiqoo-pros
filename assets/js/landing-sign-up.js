523b720b571513db4db82f5a534baac17b0efe19
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-08 12:42:02 +0300

    Promo landing on grid desktop done

--- assets/js/landing-sign-up.js
+++ assets/js/landing-sign-up.js
@@ -1,27 +1,27 @@
 const isMobile = window.innerWidth < 767;
 
-const letsGoBtn = document.querySelector('.lets-go-btn');
-const formWrapper = document.querySelector('.back-layer');
-const inner = document.querySelector('.inner');
-const body = document.querySelector('body');
-let popupVisible = false;
+// const letsGoBtn = document.querySelector('.lets-go-btn');
+// const formWrapper = document.querySelector('.back-layer');
+// const inner = document.querySelector('.inner');
+// const body = document.querySelector('body');
+// let popupVisible = false;
 
 
-letsGoBtn.addEventListener('click', function() {
-	if (isMobile) {
-		formWrapper.classList.add('is-active');
-		body.classList.add('blocked');
-		popupVisible = true;
-	} else {
-		formWrapper.classList.remove('is-active');
-		body.classList.remove('blocked');
-	}
-});
+// letsGoBtn.addEventListener('click', function() {
+// 	if (isMobile) {
+// 		formWrapper.classList.add('is-active');
+// 		body.classList.add('blocked');
+// 		popupVisible = true;
+// 	} else {
+// 		formWrapper.classList.remove('is-active');
+// 		body.classList.remove('blocked');
+// 	}
+// });
 
-inner.addEventListener('click', () => {
-	formWrapper.classList.remove('is-active');
-	body.classList.remove('blocked');
-});
+// inner.addEventListener('click', () => {
+// 	formWrapper.classList.remove('is-active');
+// 	body.classList.remove('blocked');
+// });
 
 const qualityCards = document.querySelectorAll('.quality-card');
 const scaledUpCard = document.querySelector('#carousel__slide2');
