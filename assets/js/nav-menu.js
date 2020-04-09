746d0871f53590de4d9ed3e8320e0d3a401f13fc
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-09 12:41:58 +0300

    changed navmenu. Added js on hover add class for content spacing. Replaced wrapper-c navmenu container from header

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -15,30 +15,86 @@ function closeSideBar() {
 
 // Nav menu header
 // const navMenuHeader = document.querySelector('.wrapper-c .nav-menu');
-// const navMenuItems = document.querySelectorAll(".wrapper-c .nav-menu .nav-menu-item");
-// const navSubMenu = document.querySelector(".wrapper-c .nav-sub-menu");
-// const mainRow = document.querySelector('#main-row');
-// let subMenuIsVisible = false;
+const navMenuItems = document.querySelectorAll(".wrapper-c .nav-menu .nav-menu-item");
+const navSubMenu = document.querySelector(".wrapper-c .nav-sub-menu");
+const mainRow = document.querySelector('#main-row');
+let subMenuIsVisible = false;
 // console.log('=>', navMenuHeader);
 
-// navMenuItems.forEach(item => {
-// 	item.addEventListener('mouseover', function() {
-		// // console.log('=> Worked');
-		// navSubMenu.classList.remove('is-hidden');
-		// navSubMenu.classList.add('is-active');
-		// mainRow.classList.add('nav-bar-is-opened');
-		// subMenuIsVisible = true;
+navMenuItems.forEach(item => {
+	if (navSubMenu.classList.contains('is-active')) {
+		item.addEventListener('mouseover', function() {
+			subMenuIsVisible = true;
+			return;
+		});
+		item.addEventListener('mouseout', function() {
+			return;
+		});
+	}
+	 else {
+		item.addEventListener('mouseover', function() {
+			navSubMenu.classList.remove('is-hidden');
+			navSubMenu.classList.add('is-active');
+			mainRow.classList.add('nav-bar-is-opened');
+			subMenuIsVisible = true;
+		});
+		item.addEventListener('mouseout', function() {
+			navSubMenu.classList.remove('is-active');
+			navSubMenu.classList.add('is-hidden');
+			mainRow.classList.remove('nav-bar-is-opened');
+			subMenuIsVisible = false;
+		});
+	}
+
+
+	// item.addEventListener('mouseover', function() {
+	// 	// console.log('=> Worked');
+	//
+	// 	if (navSubMenu.classList.contains('is-active')) {
+	// 		subMenuIsVisible = true;
+	// 		return;
+	// 		// mainRow.classList.add('nav-bar-is-opened');
+	// 	} else {
+	// 		navSubMenu.classList.remove('is-hidden');
+	// 		navSubMenu.classList.add('is-active');
+	// 		mainRow.classList.add('nav-bar-is-opened');
+	// 		// subMenuIsVisible = true;
+	// 		// mainRow.classList.remove('nav-bar-is-opened');
+	// 	}
+	// 	//
+	// 	// navSubMenu.classList.remove('is-hidden');
+	// 	// navSubMenu.classList.add('is-active');
+	// 	// mainRow.classList.add('nav-bar-is-opened');
+	// 	// subMenuIsVisible = true;
 	// });
 	// item.addEventListener('mouseout', function() {
-		// console.log('=> Worked');
-		// navSubMenu.classList.add('is-hidden');
-		// navSubMenu.classList.remove('is-active');
-		// mainRow.classList.remove('nav-bar-is-opened');
-		// subMenuIsVisible = false;
-// 	});
-// });
+	//
+	// 	if (navSubMenu.classList.contains('is-active')) {
+	// 		return;
+	// 		// navSubMenu.classList.add('is-hidden');
+	// 		// mainRow.classList.remove('nav-bar-is-opened');
+	// 	} else {
+	// 		navSubMenu.classList.remove('is-active');
+	// 		navSubMenu.classList.add('is-hidden');
+	// 		mainRow.classList.remove('nav-bar-is-opened');
+	// 		subMenuIsVisible = false;
+	// 	}
+	//
+	// 	if (!navSubMenu.classList.contains('is-active')) {
+	//
+	// 	}
+	// 	//
+	// 	// navSubMenu.classList.add('is-hidden');
+	// 	// navSubMenu.classList.remove('is-active');
+	// 	// mainRow.classList.remove('nav-bar-is-opened');
+	// });
+});
 
-
+if (navSubMenu.classList.contains('is-active')) {
+	mainRow.classList.add('nav-bar-is-opened');
+} else {
+	mainRow.classList.remove('nav-bar-is-opened');
+}
 
 // EOL: Igor
 
