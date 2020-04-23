d5aa7c0a38c0f64f366acc3ba6c69d1e0451817b
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-23 18:23:48 +0300

    area settings page desktop almost done. New layout in progress.

--- assets/js/new-page-layout.js
+++ assets/js/new-page-layout.js
@@ -3,9 +3,16 @@ const dropdownList = document.querySelector('.dropdown-list');
 
 notificationButtons.forEach(button => {
 	button.addEventListener('click', function () {
-		console.log(this, '1111111');
-		this.classList.add('is-active');
-		dropdownList.classList.remove('invisible');
-		dropdownList.classList.add('visible');
+		if(!this.classList.contains('is-active')) {
+			// console.log(this, '1111111');
+			this.classList.add('is-active');
+			dropdownList.classList.remove('invisible');
+			dropdownList.classList.add('visible');
+		} else {
+			// console.log(this, '2222222');
+			this.classList.remove('is-active');
+			dropdownList.classList.remove('visible');
+			dropdownList.classList.add('invisible');
+		}
 	});
 });
