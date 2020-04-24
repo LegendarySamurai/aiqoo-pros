48dc25e42c1d82a3ce5e0a8fb4adf953742e6e4a
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-24 15:54:42 +0300

    dropdown info for user on click is done

--- assets/js/new-page-layout.js
+++ assets/js/new-page-layout.js
@@ -1,18 +1,32 @@
 const notificationButtons = document.querySelectorAll('.notification-button');
+const userInfoTop = document.querySelector('.info-inner');
 const dropdownList = document.querySelector('.dropdown-list');
+const dropdownInfo = document.querySelector('.dropdown-info');
 
 notificationButtons.forEach(button => {
 	button.addEventListener('click', function () {
 		if(!this.classList.contains('is-active')) {
-			// console.log(this, '1111111');
 			this.classList.add('is-active');
 			dropdownList.classList.remove('invisible');
 			dropdownList.classList.add('visible');
 		} else {
-			// console.log(this, '2222222');
 			this.classList.remove('is-active');
 			dropdownList.classList.remove('visible');
 			dropdownList.classList.add('invisible');
 		}
 	});
+});
+
+userInfoTop.addEventListener('click', function() {
+	if(!userInfoTop.classList.contains('is-active')) {
+		// console.log(this, '1111111');
+		userInfoTop.classList.add('is-active');
+		dropdownInfo.classList.remove('invisible');
+		dropdownInfo.classList.add('visible');
+	} else {
+		// console.log(this, '2222222');
+		this.classList.remove('is-active');
+		dropdownInfo.classList.remove('visible');
+		dropdownInfo.classList.add('invisible');
+	}
 });
