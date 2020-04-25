a8b4155dc52011f5bf03c3d389a6a90e3ece581d
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-25 20:11:39 +0300

    Changed user button structure and effects, added dropdown menu for it. Added active state for sub-menu items on click event. For each separate item in user menu added separate dropdown.

--- assets/js/new-page-layout.js
+++ assets/js/new-page-layout.js
@@ -1,32 +1,74 @@
-const notificationButtons = document.querySelectorAll('.notification-button');
-const userInfoTop = document.querySelector('.info-inner');
-const dropdownList = document.querySelector('.dropdown-list');
+
+// On top fixed panel users buttons click - showes dropdown for each.
+const notificationButtonOne = document.querySelector('.notification-1 .notification-button');
+const notificationButtonTwo = document.querySelector('.notification-2 .notification-button');
+const userInfoTop = document.querySelector('.user');
+const dropdownListOne = document.querySelector('.dropdown-list-one');
+const dropdownListTwo = document.querySelector('.dropdown-list-two');
 const dropdownInfo = document.querySelector('.dropdown-info');
 
-notificationButtons.forEach(button => {
-	button.addEventListener('click', function () {
-		if(!this.classList.contains('is-active')) {
-			this.classList.add('is-active');
-			dropdownList.classList.remove('invisible');
-			dropdownList.classList.add('visible');
-		} else {
-			this.classList.remove('is-active');
-			dropdownList.classList.remove('visible');
-			dropdownList.classList.add('invisible');
-		}
-	});
+notificationButtonOne.addEventListener('click', function () {
+	if(!this.classList.contains('is-active')) {
+		// console.log(this, '1111111');
+		this.classList.add('is-active');
+		dropdownListOne.classList.remove('invisible');
+		dropdownListOne.classList.add('visible');
+	} else {
+
+		// console.log(this, '2222222');
+		this.classList.remove('is-active');
+		dropdownListOne.classList.remove('visible');
+		dropdownListOne.classList.add('invisible');
+	}
+});
+
+notificationButtonTwo.addEventListener('click', function () {
+	if(!this.classList.contains('is-active')) {
+
+		// console.log(this, '3333');
+		this.classList.add('is-active');
+		dropdownListTwo.classList.remove('invisible');
+		dropdownListTwo.classList.add('visible');
+	} else {
+
+		// console.log(this, '44444');
+		this.classList.remove('is-active');
+		dropdownListTwo.classList.remove('visible');
+		dropdownListTwo.classList.add('invisible');
+	}
 });
 
 userInfoTop.addEventListener('click', function() {
 	if(!userInfoTop.classList.contains('is-active')) {
-		// console.log(this, '1111111');
+		// console.log(this, '5555555');
 		userInfoTop.classList.add('is-active');
 		dropdownInfo.classList.remove('invisible');
 		dropdownInfo.classList.add('visible');
 	} else {
-		// console.log(this, '2222222');
+		// console.log(this, '666666');
 		this.classList.remove('is-active');
 		dropdownInfo.classList.remove('visible');
 		dropdownInfo.classList.add('invisible');
 	}
+});
+
+
+// Adds class "is-active" to the clicked sub-menu item
+const subMenuItems = document.querySelectorAll('.sub-menu-item');
+let subMenuBtnClicked = false;
+
+subMenuItems.forEach(item => {
+	item.addEventListener('click', () => {
+		subMenuItems.forEach(item => {
+			item.classList.remove('is-active');
+		});
+
+		if (!item.classList.contains('is-active')) {
+			item.classList.add('is-active');
+			subMenuBtnClicked = true;
+		} else {
+			item.classList.remove('is-active');
+			subMenuBtnClicked = false;
+		}
+	});
 });
