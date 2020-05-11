d5a4eecf3b65c02e591af70e2433883c8ce78c6a
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-05-11 19:07:43 +0300

    added modal on terms and conditions click

--- assets/js/marketing-signup.js
+++ assets/js/marketing-signup.js
@@ -169,8 +169,15 @@ function onWheel(e) {
 // Terms and conditions popup
 const termsAndConditionsOpenBtn = document.querySelector('.open-terms-and-conditions');
 const termAndConditionsInfoModal = document.querySelector('.terms-and-conditions-info');
+const modalCloseBtn = document.querySelector('.terms-and-conditions-info .close');
+const modalBackdrop = document.querySelector('.modal-backdrop');
 
-termsAndConditionsOpenBtn.addEventListener('click', function(e) {
-	console.log(e.target);
+termsAndConditionsOpenBtn.addEventListener('click', function() {
 	termAndConditionsInfoModal.classList.add('active');
+	modalBackdrop.classList.add('active');
+});
+
+modalCloseBtn.addEventListener('click', function() {
+	termAndConditionsInfoModal.classList.remove('active');
+	modalBackdrop.classList.remove('active');
 });
