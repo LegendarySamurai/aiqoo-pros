d3785b7520545791489bee106048d6f5ee071cbf
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-05-12 15:28:36 +0300

    checkboxes, submit buttons and modals for Terms and conditions on marketing, sign-in, sign-up pages

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,27 @@
+const submitSignUpBtn = document.querySelector('#submit-sign-up');
 const submitSignInBtn = document.querySelector('#submit-sign-up');
 
 submitSignInBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 });
+
+submitSignUpBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+});
+
+// Terms and conditions popup
+const termsAndConditionsOpenBtn = document.querySelector('.open-terms-and-conditions');
+const termAndConditionsInfoModal = document.querySelector('.terms-and-conditions-info');
+const modalCloseBtn = document.querySelector('.terms-and-conditions-info .close');
+const modalBackdrop = document.querySelector('.modal-backdrop');
+
+termsAndConditionsOpenBtn.addEventListener('click', function() {
+	termAndConditionsInfoModal.classList.add('active');
+	modalBackdrop.classList.add('active');
+});
+
+modalCloseBtn.addEventListener('click', function() {
+	termAndConditionsInfoModal.classList.remove('active');
+	modalBackdrop.classList.remove('active');
+});
+
