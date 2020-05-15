340ae4d74bcdd9e124e1cc50e051a570f7adb06a
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-05-15 20:14:42 +0300

    Leads list page all devices done

--- assets/js/custom-popover.js
+++ assets/js/custom-popover.js
@@ -1,6 +1,4 @@
-// Created by Igor March 5th 2020
-
-// popover-one
+// Add portfolio page popovers
 $('#popover-one').popover({
 	trigger: 'hover',
 	placement: 'top'
@@ -17,4 +15,57 @@ $('#popover-three').popover({
 });
 
 
+// Alert popover (Leads list page)
+$('.alert-notification').popover({
+	trigger: 'hover'
+});
+// EOL: Alert popover (Leads list page)
 
+// // Atempt dropdown (Leads list page)
+// const button = document.querySelector('#atempts-button');
+// const tooltip = document.querySelector('#atempts-tooltip');
+//
+// let popperInstance = null;
+//
+// function create() {
+// 	popperInstance = Popper.createPopper(button, tooltip, {
+// 		modifiers: [
+// 			{
+// 				name: 'offset',
+// 				options: {
+// 					offset: [0, 2],
+// 				},
+// 			},
+// 		],
+// 	});
+// }
+//
+// function destroy() {
+// 	if (popperInstance) {
+// 		popperInstance.destroy();
+// 		popperInstance = null;
+// 	}
+// }
+//
+// function show() {
+// 	tooltip.setAttribute('data-show', '');
+// 	create();
+// }
+//
+// function hide() {
+// 	tooltip.removeAttribute('data-show');
+// 	destroy();
+// }
+//
+// const showEvents = ['mouseenter', 'focus'];
+// const hideEvents = ['mouseleave', 'blur'];
+//
+// showEvents.forEach(event => {
+// 	button.addEventListener(event, show);
+// });
+//
+// hideEvents.forEach(event => {
+// 	button.addEventListener(event, hide);
+// });
+
+// EOL: Atempt dropdown (Leads list page)
