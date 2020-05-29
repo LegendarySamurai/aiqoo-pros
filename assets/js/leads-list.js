ca09614854bb18716cdce9720e18d6960dd90dfb
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-05-29 20:21:15 +0300

    Leads list popup in progress

--- assets/js/leads-list.js
+++ assets/js/leads-list.js
@@ -1,4 +1,27 @@
-$('.toggleTab').on('click', function(e) {
-	e.preventDefault();
+window.toggleList = (e) => {
+	// var width = $(window).width();
+	if ($(e).hasClass("active")) {
+		$(e).hide().removeClass("active");
+		return;
+	}
 
-});
+	if ($(".toggle-panel").hasClass("active")) {
+		$(".toggle-panel").hide().removeClass("active");
+		$(e).addClass("active").show();
+	}	else {
+		$(".toggle-panel").removeClass("active");
+		$(e).addClass("active");
+		$(e).show();
+	}
+};
+
+window.addEventListener("resize", myFunction);
+
+function myFunction() {
+	let width = $(window).width();
+	if (width >= 1300) {
+		$(".toggle-panel").show();
+	} else {
+		$(".activity-list").addClass("active");
+	}
+}
