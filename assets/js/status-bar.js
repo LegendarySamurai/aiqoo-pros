011551d16032dab099d6b2c45396b1773928f0d8
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-06-03 12:26:57 +0300

    filter open button on the right, add filter close button, each button on panel opens filter, add margin right when filter opened on small screens.

--- assets/js/status-bar.js
+++ assets/js/status-bar.js
@@ -2,41 +2,42 @@ const filterButton = document.querySelector('.filter-btn');
 const statusBarInner = document.querySelector('.status-bar-inner');
 const closeStatusBarInnerBtn = document.querySelector('.close-filter-btn');
 const closeFilter = document.querySelector('.close-filter');
+const closeStatusBarBtn = document.querySelector('.close-status-bar-btn');
 
-const openFilterBtn = document.querySelector('.open-filter-btn');
+const openFilterBtns = document.querySelectorAll('.open-filter');
 const statusBarCol = document.querySelector('.status-bar-col');
 
-let width = $(window).width();
-if (width < 600) {
-	$(".status-bar-inner").removeClass("is-active");
+if (window.innerWidth < 600) {
+	statusBarInner.classList.remove("is-active");
 
-	$(".filter-btn").addEventListener("click", () => {
-		$(".status-bar-inner").addClass("is-active");
+	filterButton.addEventListener("click", () => {
+		statusBarInner.classList.add("is-active");
 		// statusBarBacklayer.classList.add('is-active');
 	});
 
-	$(".close-filter-btn").addEventListener("click", () => {
-		$(".status-bar-inner").removeClass("is-active");
+	closeStatusBarInnerBtn.addEventListener("click", () => {
+		console.log('=> Works');
+		statusBarInner.classList.remove("is-active");
 	});
 
-	$(".close-filter").addEventListener("click", () => {
-		$(".status-bar-inner").removeClass("is-active");
+	closeFilter.addEventListener("click", () => {
+		statusBarInner.classList.remove("is-active");
 	});
 }
 
 if (window.innerWidth >= 600) {
-	// statusBarCol.classList.remove('filter-closed');
-	// statusBarCol.classList.remove('filter-opened');
-	openFilterBtn.addEventListener('click', () => {
-		if (statusBarCol.classList.contains('filter-closed')) {
-			// console.log(1);
-			statusBarCol.classList.add('filter-opened');
-			statusBarCol.classList.remove('filter-closed');
-		} else {
-			// console.log(2);
-			statusBarCol.classList.remove('filter-opened');
-			statusBarCol.classList.add('filter-closed');
-		}
+	openFilterBtns.forEach(button => {
+		button.addEventListener('click', () => {
+			if (statusBarCol.classList.contains('filter-closed')) {
+				statusBarCol.classList.add('filter-opened');
+				statusBarCol.classList.remove('filter-closed');
+			}
+		});
+	});
+
+	closeStatusBarBtn.addEventListener('click', () => {
+		statusBarCol.classList.remove('filter-opened');
+		statusBarCol.classList.add('filter-closed');
 	});
 }
 
