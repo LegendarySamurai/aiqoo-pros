696dbeea91dfd26f23b535d8d7b7d7de50dc3337
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-06-10 17:28:19 +0300

    filter scroll js in progress

--- assets/js/leads-list.js
+++ assets/js/leads-list.js
@@ -36,42 +36,80 @@ function myFunction() {
 
 // let windowHeight = $(window).height();
 let tableRowList = document.querySelector('.table-row-list');
-// let leadsList = document.querySelector('.leads-list');
-// let leadsListHeight = leadsList.offsetHeight;
-// let tableRowListHeight = tableRowList.scrollHeight;
-// let pageBottomPosition = tableRowListHeight - 92 - 60 - 30;
-// console.log("pageBottomPosition1", pageBottomPosition);
-// console.log("leadsListHeight", leadsListHeight);
-// const statusBarCol = document.querySelector('.status-bar-col');
+let tableRowListHeight = tableRowList.clientHeight;
 const leadsListCol = document.querySelector('.leads-list-col');
-let statusBarHeight = document.querySelector('.status-bar');
 const header = document.querySelector('.fixed-top-panel-d');
+let statusBar = document.querySelector('.status-bar');
+let statusBarHeight = statusBar.clientHeight;
 const headerHeight = header.clientHeight;
 const search = document.querySelector('.search');
 const searchHeight = search.clientHeight;
+
+
+// function gotoBottom(className){
+// 	let element = document.getElementsByClassName(className);
+// 	element.scrollTop = element.scrollHeight - element.clientHeight;
+// }
+
 
 document.addEventListener('scroll', () => {
 	let scroll = $(document).scrollTop();
 	let leftContentHeight = statusBarCol.innerHeight;
 	let rightContentHeight = leadsListCol.innerHeight;
-	console.log('true or false', leftContentHeight < rightContentHeight);
+	// console.log('=>', statusBarHeight > tableRowListHeight);
 
+	console.log('SCROLL', scroll);
 	const contentHeight = tableRowList.clientHeight - window.innerHeight + headerHeight + searchHeight + 40;
 
-	if(scroll > contentHeight) {
-		console.log(contentHeight, scroll);
-		// tableRowList.classList.add('fixed-position');
-		if(!statusBarCol.classList.contains('filter-opened')) {
-			return;
-		} else {
+	if(statusBarCol.classList.contains('filter-opened')) {
+		console.log('Filter is open!');
+		if (statusBarHeight > tableRowListHeight) {
 			tableRowList.classList.add('fixed-position');
+			console.log('1.fixed position added');
+		} else {
+			console.log('1');
+			tableRowList.classList.remove('fixed-position');
 		}
+	} else {
+		console.log('Filter is close!');
+		if (statusBarHeight > tableRowListHeight) {
+			// console.log('status bar High!');
+			console.log(statusBarHeight, tableRowListHeight);
+			tableRowList.classList.add('fixed-position');
+			console.log('2.fixed position added');
+		} else {
+			console.log('Status bar is NOT High!');
+			tableRowList.classList.remove('fixed-position');
+		}
+	}
 
+	if (statusBarHeight > tableRowListHeight) {
+		// console.log(statusBarHeight, tableRowListHeight);
+		// if(scroll > contentHeight) {
+		// 	console.log(contentHeight, scroll);
+		// 	// tableRowList.classList.add('fixed-position');
+		// 	// if(!statusBarCol.classList.contains('filter-opened')) {
+		// 	// 	return;
+		// 	// } else {
+		// 	// 	tableRowList.classList.add('fixed-position');
+		// 	// }
+		//
+		// }
 	}
-	else {
-		console.log(2);
-		tableRowList.classList.remove('fixed-position');
-	}
+	// if(scroll > contentHeight) {
+	// 	console.log(contentHeight, scroll);
+	// 	// tableRowList.classList.add('fixed-position');
+	// 	if(!statusBarCol.classList.contains('filter-opened')) {
+	// 		return;
+	// 	} else {
+	// 		tableRowList.classList.add('fixed-position');
+	// 	}
+	//
+	// }
+	// else {
+	// 	console.log(2);
+	// 	tableRowList.classList.remove('fixed-position');
+	// }
 
 	// console.log(
 	// 	"=>",
